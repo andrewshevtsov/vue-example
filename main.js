850293b6aa2b4669c68efdf8fa34826346576b67
andrewshevtsov <andrewshevtsov@inbox.ru>
2019-03-07 00:46:47 +0300

    vue-component was added

--- main.js
+++ main.js
@@ -1,5 +1,55 @@
+/* ============================================ */
+
+
+
+var UsersTable = {
+  template: '#users-table',
+  props: {
+    users: {
+      type: Array,
+      required: true
+    }
+  },
+  filters: {
+    toUppercase(value) {
+      if (!value) return ''
+      value = value.toString();
+      return value.toUpperCase();
+    }
+  },
+  data: function() {
+    return {
+      test: 123,
+      isListVisible: true
+    }
+  },
+  computed: {
+    abc: function() {
+      return this.test + 4
+    },
+    totalUsers: function() {
+      return this.users.length;
+    }
+  },
+  methods: {
+    remove: function(id) {
+      console.log('component remove', id);
+      this.$emit('remove', id);
+    }
+  }
+}
+
+
+
+/* ============================================ */
+
+
+
 const app = new Vue({
   el: '#app',
+  components: {
+    'users-table': UsersTable
+  },
   data: function () {
     return {
       title: 'Some title',
@@ -13,17 +63,25 @@ const app = new Vue({
       return 'https://via.placeholder.com/100';
     }
   },
-  filters: {
-    toUppercase(value) {
-      if (!value) return ''
-      value = value.toString();
-      return value.toUpperCase();
+  watch: {
+    isListVisible: {
+      // deep: true,
+      immediate: true,
+      handler: function() {
+        console.log('Чекаво жучара, кнопку нажимал?', this.isListVisible);
+      }
     }
   },
   mounted: function() {
     this.loadUsers();
   },
   methods: {
+    removeFromList(id) {
+      console.log('parent remove', id);
+      this.list = this.list.filter(function(item) {
+        return item.id !== id
+      });
+    },
     totalUsers() {
       return this.list.length;
     },
